2d72e189ab2df2bbd8026700ca37798278bde177
qwertymol <strajcko2@gmail.com>
2018-02-14 22:56:42 +0300

    created

---
+++ windows1251encoding.cpp
@@ -0,0 +1,6 @@
+#include "windows1251encoding.h"
+
+Windows1251Encoding::Windows1251Encoding()
+{
+
+}
---
+++ windows1251encoding.h
@@ -0,0 +1,11 @@
+#ifndef WINDOWS1251ENCODING_H
+#define WINDOWS1251ENCODING_H
+
+
+class Windows1251Encoding
+{
+public:
+    Windows1251Encoding();
+};
+
+#endif // WINDOWS1251ENCODING_H
